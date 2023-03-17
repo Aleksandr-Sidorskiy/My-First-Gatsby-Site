ccbf394551a569d99d89fb3883e13b659dc398e6
Aleksandr-Sidorskiy <you@example.comsanu43967@gmail.com>
2023-03-17 20:32:08 +0200

    setting json homepage/repozitoriy

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,6 @@
 module.exports = {
   siteMetadata: {
     title: `My First Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: ["gatsby-plugin-styled-components"],
 };
